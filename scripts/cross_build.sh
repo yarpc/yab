4f233a7f79a6d97177e5f98a30a98f70bc42ab0d
Prashant Varanasi <prashant@uber.com>
2016-07-18 23:42:40 -0700

    Add script to create prebuilt yab binaries for the release page

---
+++ scripts/cross_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p build
+for GOOS in linux darwin
+do
+	for GOARCH in amd64
+		do
+		echo Building $GOOS/$GOARCH
+		GOOS=$GOOS GOARCH=$GOARCH go build -o build/yab-$GOOS-$GOARCH .
+    zip build/yab-$GOOS-$GOARCH.zip build/yab-$GOOS-$GOARCH
+	done
+done
+
