97bc4809b30b17a348c538b3bb9f9c42a29f7847
Jeremy Suurkivi <jeremy.suurkivi@uber.com>
2016-11-14 15:33:43 -0800

    WIP

--- template_test.go
+++ template_test.go
@@ -21,50 +21,24 @@
 package main
 
 import (
-	"io/ioutil"
-	"time"
-
-	"gopkg.in/yaml.v2"
+	"testing"
+	"code.uber.internal/rt/carlton/Godeps/_workspace/src/gopkg.in/yaml.v2"
 )
 
-type template struct {
-	Service string            `yaml:"service"`
-	Thrift  string            `yaml:"thrift"`
-	Method  string            `yaml:"method"`
-	Headers map[string]string `yaml:"headers"`
-	Request interface{}       `yaml:"request"`
-	Timeout time.Duration     `yaml:"timeout"`
-}
+func TestTemplate(t *testing.T) {
+	data := `
+		service: timer
+		thrift: testdata/templates/foo.thrift
+		method: Simple:foo
+		headers:
+			header1: abc
+		request:
+			location:
+				latitude: 37.7
+				longitude: -122.4
+		timeout: 5s
+	`
 
-func readYamlRequest(opts *Options) error {
 	t := template{}
-
-	bytes, err := ioutil.ReadFile(opts.ROpts.YamlTemplate)
-	if err != nil {
-		return err
-	}
-
-	err = yaml.Unmarshal(bytes, &t)
-	if err != nil {
-		return err
-	}
-
-	body, err := yaml.Marshal(t.Request)
-	if err != nil {
-		return err
-	}
-
-	headers, err := yaml.Marshal(t.Headers)
-	if err != nil {
-		return err
-	}
-
-	opts.ROpts.ThriftFile = t.Thrift
-	opts.ROpts.MethodName = t.Method
-	opts.TOpts.ServiceName = t.Service
-	opts.ROpts.HeadersJSON = string(headers)
-	opts.ROpts.RequestJSON = string(body)
-	opts.ROpts.Timeout = timeMillisFlag(t.Timeout)
-
-	return nil
+	yaml.Unmarshal(data, &t)
 }
