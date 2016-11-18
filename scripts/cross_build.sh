c26cf921773424dea41a3f527c807a8e6409e6e4
Prashant Varanasi <prashant@uber.com>
2016-11-18 15:06:56 -0800

    Spaces!!

--- scripts/cross_build.sh
+++ scripts/cross_build.sh
@@ -3,11 +3,11 @@
 mkdir -p build
 for GOOS in linux darwin
 do
-	for GOARCH in amd64
-		do
-		echo Building $GOOS/$GOARCH
-		GOOS=$GOOS GOARCH=$GOARCH go build -o build/yab-$GOOS-$GOARCH .
+  for GOARCH in amd64
+    do
+    echo Building $GOOS/$GOARCH
+    GOOS=$GOOS GOARCH=$GOARCH go build -o build/yab-$GOOS-$GOARCH .
     zip build/yab-$GOOS-$GOARCH.zip build/yab-$GOOS-$GOARCH
-	done
+  done
 done
 
