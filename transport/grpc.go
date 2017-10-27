7fc6b04f5fc854bd26b34b667e391585ac13703e
Peter Edge <pedge@uber.com>
2017-10-27 15:35:52 +0200

    Add grpc transport

---
+++ transport/grpc.go
@@ -0,0 +1,188 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package transport
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io/ioutil"
+	"time"
+
+	"github.com/opentracing/opentracing-go"
+	"go.uber.org/multierr"
+	apipeer "go.uber.org/yarpc/api/peer"
+	"go.uber.org/yarpc/api/transport"
+	"go.uber.org/yarpc/peer"
+	"go.uber.org/yarpc/peer/hostport"
+	"go.uber.org/yarpc/peer/roundrobin"
+	"go.uber.org/yarpc/transport/grpc"
+)
+
+var (
+	errGRPCNoAddresses = errors.New("must specify at least one grpc address")
+	errGRPCNoTracer    = errors.New("must specify grpc tracer")
+	errGRPCNoCaller    = errors.New("must specify grpc caller")
+	errGRPCNoService   = errors.New("must specify grpc service")
+	errGRPCNoEncoding  = errors.New("must specify grpc encoding")
+	errGRPCNoProcedure = errors.New("must specify grpc procedure")
+)
+
+// GRPCOptions are used to create a GRPC transport.
+type GRPCOptions struct {
+	Addresses       []string
+	Tracer          opentracing.Tracer
+	Caller          string
+	Encoding        string
+	RoutingKey      string
+	RoutingDelegate string
+}
+
+// NewGRPC returns a transport that calls a GRPC service.
+func NewGRPC(options GRPCOptions) (TransportCloser, error) {
+	return newGRPC(options)
+}
+
+type grpcTransport struct {
+	Transport       transport.Transport
+	Outbound        transport.UnaryOutbound
+	Caller          string
+	Encoding        string
+	RoutingKey      string
+	RoutingDelegate string
+	tracer          opentracing.Tracer
+}
+
+func newGRPC(options GRPCOptions) (*grpcTransport, error) {
+	if len(options.Addresses) == 0 {
+		return nil, errGRPCNoAddresses
+	}
+	if options.Tracer == nil {
+		return nil, errGRPCNoTracer
+	}
+	if options.Caller == "" {
+		return nil, errGRPCNoCaller
+	}
+	if options.Encoding == "" {
+		return nil, errGRPCNoEncoding
+	}
+
+	identifiers := make([]apipeer.Identifier, len(options.Addresses))
+	for i, address := range options.Addresses {
+		identifiers[i] = hostport.Identify(address)
+	}
+	transport := grpc.NewTransport(grpc.Tracer(options.Tracer))
+	outbound := transport.NewOutbound(peer.Bind(roundrobin.New(transport), peer.BindPeers(identifiers)))
+	if err := transport.Start(); err != nil {
+		return nil, err
+	}
+	if err := outbound.Start(); err != nil {
+		_ = transport.Stop()
+		return nil, err
+	}
+
+	return &grpcTransport{
+		Transport:       transport,
+		Outbound:        outbound,
+		Caller:          options.Caller,
+		Encoding:        options.Encoding,
+		RoutingKey:      options.RoutingKey,
+		RoutingDelegate: options.RoutingDelegate,
+		tracer:          options.Tracer,
+	}, nil
+}
+
+func (t *grpcTransport) Tracer() opentracing.Tracer {
+	return t.tracer
+}
+
+func (t *grpcTransport) Protocol() Protocol {
+	return GRPC
+}
+
+func (t *grpcTransport) Call(ctx context.Context, request *Request) (*Response, error) {
+	if request.TargetService == "" {
+		return nil, errGRPCNoService
+	}
+	if request.Method == "" {
+		return nil, errGRPCNoProcedure
+	}
+
+	ctx, cancel := requestContextWithTimeout(ctx, request)
+	defer cancel()
+	transportResponse, err := t.Outbound.Call(ctx, t.requestToTransportRequest(request))
+	if err != nil {
+		return nil, err
+	}
+	response, err := transportResponseToResponse(transportResponse)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func (t *grpcTransport) Close() error {
+	return multierr.Combine(t.Transport.Stop(), t.Outbound.Stop())
+}
+
+func (t *grpcTransport) requestToTransportRequest(request *Request) *transport.Request {
+	return &transport.Request{
+		Caller:          t.Caller,
+		Service:         request.TargetService,
+		Encoding:        transport.Encoding(t.Encoding),
+		Procedure:       request.Method,
+		Headers:         transport.HeadersFromMap(request.Headers),
+		ShardKey:        request.ShardKey,
+		RoutingKey:      t.RoutingKey,
+		RoutingDelegate: t.RoutingDelegate,
+		Body:            bytes.NewReader(request.Body),
+	}
+}
+
+func requestContextWithTimeout(ctx context.Context, request *Request) (context.Context, context.CancelFunc) {
+	if _, ok := ctx.Deadline(); ok {
+		return ctx, noop
+	}
+	timeout := time.Second
+	if request.Timeout > 0 {
+		timeout = request.Timeout
+	}
+	return context.WithTimeout(ctx, timeout)
+}
+
+func transportResponseToResponse(transportResponse *transport.Response) (*Response, error) {
+	response := &Response{
+		Headers: transportResponse.Headers.Items(),
+	}
+	if transportResponse.Body != nil {
+		body, err := ioutil.ReadAll(transportResponse.Body)
+		if err != nil {
+			return nil, err
+		}
+		if err := transportResponse.Body.Close(); err != nil {
+			return nil, err
+		}
+		response.Body = body
+	}
+	return response, nil
+}
+
+func noop() {}
---
+++ transport/grpc_test.go
@@ -0,0 +1,209 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package transport
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/multierr"
+	"go.uber.org/yarpc/api/transport"
+	yarpcjson "go.uber.org/yarpc/encoding/json"
+	"go.uber.org/yarpc/transport/grpc"
+	"go.uber.org/yarpc/yarpcerrors"
+)
+
+func TestGRPCSuccess(t *testing.T) {
+	doWithTestEnv(t, "example-caller", 5, []transport.Procedure{
+		newTestJSONProcedure("example", "Foo::Bar", testBar),
+	}, func(t *testing.T, testEnv *testEnv) {
+		request, err := newTestJSONRequest("example", "Foo::Bar", &testBarRequest{One: "hello"})
+		require.NoError(t, err)
+		response, err := testEnv.Transport.Call(context.Background(), request)
+		require.NoError(t, err)
+		require.NotNil(t, response)
+		testBarResponse := &testBarResponse{}
+		require.NoError(t, json.Unmarshal(response.Body, testBarResponse))
+		require.Equal(t, "hello", testBarResponse.One)
+	})
+}
+
+func TestGRPCError(t *testing.T) {
+	doWithTestEnv(t, "example-caller", 5, []transport.Procedure{
+		newTestJSONProcedure("example", "Foo::Bar", testBar),
+	}, func(t *testing.T, testEnv *testEnv) {
+		request, err := newTestJSONRequest("example", "Foo::Bar", &testBarRequest{Error: "hello"})
+		require.NoError(t, err)
+		_, err = testEnv.Transport.Call(context.Background(), request)
+		require.Equal(t, yarpcerrors.UnknownErrorf("hello"), err)
+	})
+}
+
+type testBarRequest struct {
+	One   string
+	Error string
+}
+
+type testBarResponse struct {
+	One string
+}
+
+func testBar(ctx context.Context, request *testBarRequest) (*testBarResponse, error) {
+	if request == nil {
+		return nil, nil
+	}
+	if request.Error != "" {
+		return nil, errors.New(request.Error)
+	}
+	return &testBarResponse{
+		One: request.One,
+	}, nil
+}
+
+func newTestJSONRequest(service string, method string, request interface{}) (*Request, error) {
+	body, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+	return &Request{
+		TargetService: service,
+		Method:        method,
+		Body:          body,
+	}, nil
+}
+
+func newTestJSONProcedure(service string, name string, handler interface{}) transport.Procedure {
+	procedure := yarpcjson.Procedure(name, handler)[0]
+	procedure.Service = service
+	return procedure
+}
+
+func doWithTestEnv(
+	t *testing.T,
+	caller string,
+	numInbounds int,
+	procedures []transport.Procedure,
+	f func(*testing.T, *testEnv),
+) {
+	testEnv, err := newTestEnv(caller, numInbounds, procedures)
+	require.NoError(t, err)
+	defer func() {
+		assert.NoError(t, testEnv.Close())
+	}()
+	f(t, testEnv)
+}
+
+type testEnv struct {
+	Caller         string
+	Transport      *grpcTransport
+	YARPCTransport *grpc.Transport
+	YARPCInbounds  []*grpc.Inbound
+}
+
+func newTestEnv(
+	caller string,
+	numInbounds int,
+	procedures []transport.Procedure,
+) (_ *testEnv, err error) {
+	yarpcTransport := grpc.NewTransport()
+	if err := yarpcTransport.Start(); err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err != nil {
+			err = multierr.Append(err, yarpcTransport.Stop())
+		}
+	}()
+
+	addresses := make([]string, numInbounds)
+	yarpcInbounds := make([]*grpc.Inbound, numInbounds)
+	for i := 0; i < numInbounds; i++ {
+		listener, err := net.Listen("tcp", "127.0.0.1:0")
+		if err != nil {
+			return nil, err
+		}
+		addresses[i] = listener.Addr().String()
+		yarpcInbound := yarpcTransport.NewInbound(listener)
+		yarpcInbound.SetRouter(newTestRouter(procedures))
+		if err := yarpcInbound.Start(); err != nil {
+			return nil, err
+		}
+		defer func() {
+			if err != nil {
+				err = multierr.Append(err, yarpcInbound.Stop())
+			}
+		}()
+		yarpcInbounds[i] = yarpcInbound
+	}
+
+	transport, err := newGRPC(GRPCOptions{
+		Addresses: addresses,
+		Tracer:    opentracing.NoopTracer{},
+		Caller:    caller,
+		Encoding:  "json",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &testEnv{
+		caller,
+		transport,
+		yarpcTransport,
+		yarpcInbounds,
+	}, nil
+}
+
+func (e *testEnv) Close() error {
+	err := e.Transport.Close()
+	for _, yarpcInbound := range e.YARPCInbounds {
+		err = multierr.Combine(err, yarpcInbound.Stop())
+	}
+	return multierr.Combine(err, e.YARPCTransport.Stop())
+}
+
+type testRouter struct {
+	procedures []transport.Procedure
+}
+
+func newTestRouter(procedures []transport.Procedure) *testRouter {
+	return &testRouter{procedures}
+}
+
+func (r *testRouter) Procedures() []transport.Procedure {
+	return r.procedures
+}
+
+func (r *testRouter) Choose(_ context.Context, request *transport.Request) (transport.HandlerSpec, error) {
+	for _, procedure := range r.procedures {
+		if procedure.Service == request.Service && procedure.Name == request.Procedure {
+			return procedure.HandlerSpec, nil
+		}
+	}
+	return transport.HandlerSpec{}, fmt.Errorf("no procedure for service %s and name %s", request.Service, request.Procedure)
+}
