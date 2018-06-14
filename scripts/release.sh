b545ea130f8aff4f970ebd09db96ef2993645e6a
Prashant Varanasi <github@prashantv.com>
2018-06-14 14:02:28 -0700

    Fix the release script, use curl instead of ghr (#236)
    Use curl directly instead to cut releases and upload releases.
    
    This fixes the release issues (401s) we observed when cutting the latest
    release.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,27 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
 
-if [ -z "$GITHUB_TOKEN" ]; then
-  echo "GITHUB_TOKEN is not set"
-  exit 1
+die() {
+    echo "$@" >&2
+    exit 1
+}
+
+if [ -z "${GITHUB_USER:-}" ]; then
+  die "GITHUB_USER is not set."
 fi
 
-VERSION="$1"
+if [ -z "${GITHUB_REPO:-}" ]; then
+  die "GITHUB_REPO is not set."
+fi
+
+if [ -z "${GITHUB_TOKEN:-}" ]; then
+  die "GITHUB_TOKEN is not set."
+fi
+
+VERSION="${1:-}"
 if [ -z "$VERSION" ]; then
-  echo "USAGE: $0 VERSION"
-  exit 1
+  die "USAGE: $0 VERSION"
 fi
 
 ./scripts/cross_build.sh "$VERSION"
@@ -21,15 +34,36 @@ echo "CHANGELOG:"
 echo "$CHANGELOG"
 echo ""
 
-# ghr needs either a directory or a single file so we'll move the files we
-# want to release into their own folder.
-RELEASEDIR="release-$VERSION"
-mkdir -p "$RELEASEDIR"
-mv build/yab-"$VERSION"-*.zip "$RELEASEDIR"
+UPLOADS_URL=$( \
+  echo '{}' | \
+    jq -Mr \
+      --arg version "$VERSION" \
+      --arg changelog "$CHANGELOG" \
+      '. + {
+        tag_name: $version,
+        name: $version,
+        body: $changelog,
+      }' | \
+    curl --user "$GITHUB_USER:$GITHUB_TOKEN" -X POST --data @- \
+      "https://api.github.com/repos/$GITHUB_REPO/releases" | \
+    jq -e -r '.upload_url' | \
 
-ghr \
-  -owner yarpc \
-  -token "$GITHUB_TOKEN" \
-  -body "$CHANGELOG" \
-  "$VERSION" "$RELEASEDIR"
-rm -r "$RELEASEDIR"
+    # The UPLOADS_URL has a strange {?name,label} as a suffix that we need to remove.
+    # E.g., https://uploads.github.com/repos/yarpc/yab/releases/11488347/assets{?name,label}
+    cut -d '{' -f1)
+
+# Upon creating a release, we get back the URL to which assets should be
+# uploaded under .uploads_url.
+#
+# See https://developer.github.com/v3/repos/releases/#create-a-release for
+# more information.
+
+for FILE in build/yab-"$VERSION"-*.zip; do
+  echo "Uploading $FILE"
+
+  # See https://developer.github.com/v3/repos/releases/#upload-a-release-asset
+  UPLOAD_URL="${UPLOADS_URL}?name=$(basename "$FILE")"
+  curl --user "$GITHUB_USER:$GITHUB_TOKEN" -X POST \
+    --data-binary @"$FILE" -H 'Content-Type: application/zip' \
+    "$UPLOAD_URL"
+done
